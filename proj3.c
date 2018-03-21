f6f8340547964feacac6707114ff02a6e1bc1704
Bharath T <36395100+Bharath0432@users.noreply.github.com>
2018-03-21 13:34:59 +0530

    adding code
    Input Format:
    The input consists of two lines.
    
    First line denotes N - size of array.
    Second line contains N space separated positive integers denoting the elements of array A.
    
    Output Format:
    Print a single integer, denoting the number of Odd-Even subarrays for the given array.

---
+++ proj3.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+
+int main(){
+    int n,i,count=0,count1,count2,x,s;
+    scanf("%d", &n);  
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        }
+    
+        for(int i=0;i<n;i++){
+            
+            for(int j=i;j<n;j++){
+                count1=0;
+                count2=0;
+                s=(j-i)%2;
+                if(s!=0){
+                for (int k=i; k<=j; k++){
+                     x = a[k]%2;
+                     
+                     if(x==0){
+                    count1++;
+                     }
+                    else {
+                     count2++;
+                       }
+                  }
+                  
+                 if(count1==count2){
+                count++;
+                
+                    }
+                }
+                    
+            }
+            
+        }
+            printf("%d",count);
+           
+         return 0;
+    }
+        
+   
+    
+  
+
+
